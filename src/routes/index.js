c533a528e1fa1146a8f2223c53259e39d90a65b3
Carlo Enrico <carlo.enrico@uol.com.br>
2020-04-28 00:48:58 -0300

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+// Pages
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/home" exact component={Home} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes
